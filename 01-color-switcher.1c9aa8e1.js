554391093372fb95e5d85556336550fe3c9eaa94
ElenaOnopchenko <99200667+ElenaOnopchenko@users.noreply.github.com>
2022-09-21 18:03:57 +0000

    Deploying to gh-pages from @ ElenaOnopchenko/goit-js-hw-09@a3dab88d45b39a8cc010382c8b6b957d0ed0bc60 🚀

---
+++ 01-color-switcher.1c9aa8e1.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");let r=null,a=!1;t.addEventListener("click",(()=>{r=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),a||(a=!0,t.disabled=!0)})),e.addEventListener("click",(()=>{clearInterval(r),a=!1,t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.1c9aa8e1.js.map
